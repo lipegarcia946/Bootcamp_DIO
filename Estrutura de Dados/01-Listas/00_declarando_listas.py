59a95a15dd73382ca5d64f47fa487bfd4c987216
Felipe Requena <42552805+lipegarcia946@users.noreply.github.com>
2025-11-10 15:45:28 -0300

    adicionando os arquivos de listas

---
+++ Estrutura de Dados/01-Listas/00_declarando_listas.py
@@ -0,0 +1,24 @@
+frutas = ["laranja", "maca", "uva"]
+frutas_2=["Laranja", "maca", "uva", "Pera"]
+print(frutas)
+print(frutas_2)
+
+frutas = []
+frutas_2 = []
+print(frutas)
+print(frutas_2)
+letras = list("python")
+letas_2 = list("java")
+print(letras)
+print(letas_2)
+
+numeros = list(range(10))
+numeros_2 = list(range(20))
+print(numeros)
+print(numeros_2)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+moto=["Honda","CG250",4500,2015,1550,"São Paulo", True]
+print(carro)
+print(moto)
+
